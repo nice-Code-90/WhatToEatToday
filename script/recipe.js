4b25eee1a90a823e7448975e86830520d4d830cb
Milan Mazik <mazik1990@hotmail.com>
2023-11-04 16:48:39 +0100

    API requests

--- script/recipe.js
+++ script/recipe.js
@@ -1,25 +1,52 @@
-const SubmitButton = document.querySelector(".submit");
+const SubmitButton = document.querySelector("form");
 const Search_Text = document.querySelector("#Input_Search");
 const DietFilter = document.querySelector("#diet");
 const ListOfRecipes = document.querySelector(".list_of_recipes");
-
+const $errorField = document.querySelector(".js-error");
 const API = `https://api.spoonacular.com/recipes/`;
-let IsDiet = "";
-SubmitButton.addEventListener("click", async () => {
-  debugger;
 
-  if ((DietFilter.value = "yes")) {
-    IsDiet = `&diet=vegan|vegetarian`;
+SubmitButton.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  ListOfRecipes.innerHTML = "";
+  let value = Search_Text.value.trim();
+  if (value.length > 0) {
+    $errorField.innerHTML = "";
+    if (DietFilter.value == "yes") {
+      value += `&diet=vegan|vegetarian`;
+    }
+
+    const data = await fetch(
+      `${API}complexSearch?query=${value}&apiKey=${API_KEY}`
+    );
+    const response = await data.json();
+
+    renderRecipe(response);
+  } else {
+    $errorField.innerHTML = "Empty Search Input";
   }
-  const data = await fetch(
-    `${API}complexSearch?query=${Search_Text.value}${IsDiet}&apiKey=${API_KEY}`
-  );
 
-  const response = await data.json();
+  function renderRecipe(response) {
+    let html = "";
+    if (!Array.isArray(response.results)) {
+      $errorField.innerHTML = "Nincs találat.";
+      return;
+    }
+    for (let meal of response.results) {
+      html += `
+      <section class="meal">
+        <h3>${meal.title}</h3>
+        <img 
+        src="${meal.image}"
+        alt="${meal.title}"
+        class="meal-thumbnail-img" />
 
-  ListOfRecipes.innerHTML = `
-  <div>
-  ${response.results}
-  </div>
-  `;
+      </section>
+      `;
+    }
+    if (html.length > 0) {
+      ListOfRecipes.innerHTML = html;
+    } else {
+      $errorField.innerHTML = "Nincs találat";
+    }
+  }
 });
